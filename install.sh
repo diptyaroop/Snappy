ab3ea29c8fd276f9831dd9ebc43456fe10d5eb25
Nilesh Unhale <nileshunhale96@gmail.com>
2020-02-20 20:13:41 +0530

    Added a sample solidity contract 	-- notes on how to deploy contract on ethereum

--- install.sh
+++ install.sh
@@ -1,11 +1,20 @@
-wget https://dl.google.com/go/go1.13.8.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.13.8.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
 
-git clone https://github.com/ethereum/go-ethereum.git
-cd go-ethereum/
-make geth
-./build/bin/geth #to run geth
+# --------------------- Install ethereum ------------------------- #
+sudo add-apt-repository ppa:ethereum/ethereum
+sudo apt-get update
+sudo apt-get install ethereum
+
+# =============== OR =======================
+
+# wget https://dl.google.com/go/go1.13.8.linux-amd64.tar.gz
+# sudo tar -C /usr/local -xzf go1.13.8.linux-amd64.tar.gz
+# export PATH=$PATH:/usr/local/go/bin
+
+# git clone https://github.com/ethereum/go-ethereum.git
+# cd go-ethereum/
+# make geth
+# ./build/bin/geth #to run geth
+
 
 # ================ OR ===================== 
 
@@ -14,3 +23,15 @@ make geth
 # brew upgrade
 # brew tap ethereum/ethereum
 # brew install ethereum
+
+# --------------------- Install solidity ------------------------- #
+sudo add-apt-repository ppa:ethereum/ethereum
+sudo apt-get update
+sudo apt-get install solc
+
+
+#======================== OR ================
+#sudo snap install solc
+
+
+
--- start2nodes.sh
+++ start2nodes.sh
@@ -1,33 +1,39 @@
 mkdir node1
-./go-ethereum/build/bin/geth --datadir "node1" account new
-./go-ethereum/build/bin/geth --datadir "node1" init genesis.json
-./go-ethereum/build/bin/geth --datadir "node1" --networkid 1234 --port 11111 --nodiscover console
+geth --datadir "node1" account new
+geth --datadir "node1" init genesis.json
+geth --datadir "node1" --networkid 1234 --port 11111 --nodiscover console
 
 #for > cosole commands
-> personal.listWallets
-> web3.fromWei(eth.getBalance(eth.accounts[0]), "ether"); #to check balance
+  # list wallets
+> personal.listWallets 
+
+  # get balance
+> eth.getBalance(eth.coinbase)
+> eth.getBalance(eth.accounts[0])
+> web3.fromWei(eth.getBalance(eth.accounts[0]), "ether")
+
+  # Connect to peers
 > admin.nodeInfo.enode #to get uri use this to add peers
-> admin.addPeer("enode://091b2211c90f884b3779ada638a17091c1554c857c99279cce2ad88543085bc2405dc1ef37d328488b6f82d329e0cf574f6018119e949ec495dc890962c757ea@127.0.0.1:11112?discport=0")
-> admin.peers # give info about others peers
-#transactions
-personal.unlockAccount(eth.accounts[0]) # to unlock the account 
-eth.sendTransaction({from: eth.accounts[0], to: "<wallet address on node 2>", value: 20})
-# still  node 2 will have zero balance
- miner.start()
- miner.stop()
- #get balaance
- eth.getBalance(eth.coinbase) #get balance
- eth.getBalance(eth.accounts[0])
- > web3.fromWei(eth.getBalance(eth.accounts[0]), "ether")
+> admin.addPeer("enode://341793ade41a145c1987d2ade...")
+> admin.peers # give info about connected peers
+
+  # transactions
+> personal.unlockAccount(eth.accounts[0]) # unlock account 
+> eth.sendTransaction({from: eth.accounts[0], to: 
+  "<wallet address on node 2>", value: 20})
+  # create a block 
+> miner.start()
+> miner.stop()
 
 
 
+# ------------------- OR  -------------------------- #
+# if local build
 
-
-mkdir node2
-./go-ethereum/build/bin/geth --datadir "node2" account new
-./go-ethereum/build/bin/geth --datadir "node2" init genesis.json
-./go-ethereum/build/bin/geth --datadir "node2" --networkid 1234 --port 11112 --nodiscover console
+# mkdir node2
+# ./go-ethereum/build/bin/geth --datadir "node2" account new
+# ./go-ethereum/build/bin/geth --datadir "node2" init genesis.json
+# ./go-ethereum/build/bin/geth --datadir "node2" --networkid 1234 --port 11112 --nodiscover console
 
 
 #to see all accounts
